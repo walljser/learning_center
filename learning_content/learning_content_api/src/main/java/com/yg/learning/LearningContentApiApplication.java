a49ed36deb5c3d4c180275d5ddef1f77e8952705
walljs <walljs@163.com>
2024-01-10 21:54:42 +0800

    add module:learning_content

---
+++ learning_content/learning_content_api/src/main/java/com/yg/learning/LearningContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.yg.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningContentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningContentApiApplication.class, args);
+    }
+
+}
