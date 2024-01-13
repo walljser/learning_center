958cbc803129066c7c9d87923621b020f27b9880
walljs <walljs@163.com>
2024-01-14 01:05:00 +0800

    add module: gateway

--- learning_gateway/src/main/java/com/yg/learning/GatewayApplication.java
+++ learning_gateway/src/main/java/com/yg/learning/GatewayApplication.java
@@ -1,15 +1,11 @@
 package com.yg.learning;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableSwagger2Doc
-public class LearningContentApiApplication {
-
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LearningContentApiApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
